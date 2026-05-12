ccaa1e3f467de2f47f85b5655b21dec2883c6e64
Ojo Mu'adh <ojomuadh9@gmail.com>
2026-05-12 18:09:05 +0100

    Live url

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 const router = require('./routes/todoRoutes.js');
 
 
-const live_URL = "mongodb+srv://Protegido:PROGEN6@cluster0.2vofczk.mongodb.net/AuthenticationDB?appName=Cluster0"
+const live_URL = process.env.MONGODB_URI || "mongodb+srv://Protegido:PROGEN6@cluster0.2vofczk.mongodb.net/AuthenticationDB?appName=Cluster0"
 //const local_URL = "mongodb://localhost:27017/AuthenticationDB"
 //mongodb://localhost:27017 = is the default connection string for MongoDB running on the local machine
 
 //connect to the database
-mongoose.connect(process.env.live_URL)
+mongoose.connect(live_URL)
 //mongoose.connect(local_URL) 
 //if the connection is successful
 .then(() => console.log("MongoDB Connected")) 
